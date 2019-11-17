74f3105c7869ba64ae4706ed5d85ca8e2ca23dae
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-11-17 17:31:15 +0800

    更新

---
+++ Assets/Project/Script/PMOpenFolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+    public class PMOpenFolder : FsmStateAction
+    {
+        
+        /*[RequiredField]
+        [UIHint(UIHint.TextArea)]
+        public FsmString Path;*/
+    
+        public override void OnEnter()
+        {
+            //System.Diagnostics.Process.Start(Path.Value);
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
+            Finish();
+        }
+        
+
+    }
+}
